12cc1a3f776acb73bd01a4ba6c85aa36bcac5083
Marco Buratti <ocr@Marcos-MacBook-Pro.local>
2021-04-02 13:31:45 +0200

    News Update

--- app.py
+++ app.py
@@ -297,7 +297,6 @@ def personalArea():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
 if __name__ == '__main__':  
